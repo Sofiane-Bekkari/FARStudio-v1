7cf8859b7e5fffc25c8093f5b4d66d1c5033c238
unknown <sofian.bekkari@outlook.com>
2025-05-11 18:37:55 +0100

    fix static-files++

--- farstudio/wsgi.py
+++ farstudio/wsgi.py
@@ -13,4 +13,10 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'farstudio.settings')
 
+from farstudio.farstudio.settings import BASE_DIR
+from whitenoise import WhiteNoise
+
 application = get_wsgi_application()
+application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'staticfiles'))
+
+#application = get_wsgi_application()
